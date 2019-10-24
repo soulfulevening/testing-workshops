96ed10795b39643ee5bb147110bc76c776114ae1
soulfulevening <soulfulevening@syneforge.com>
2019-10-24 21:13:02 +0300

    prepare simple form

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,20 +2,32 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\Type\SubscriptionType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
 {
+
     /**
      * @Route("/", name="homepage")
+     * @param Request $request
+     *
+     * @return Response
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        $form = $this->createForm(SubscriptionType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formData = $form->getData();
+        }
+
+        return $this->render('@App/default/index.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
