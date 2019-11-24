f6f182849d31252c6d70ff63f43acdeb08c13bb9
Oleg Kostiushko <o.kostyshko@gmail.com>
2019-11-24 22:26:37 +0200

    prepare base crud

--- src/AdminBundle/Controller/DefaultController.php
+++ src/AdminBundle/Controller/DefaultController.php
@@ -2,19 +2,58 @@
 
 namespace AdminBundle\Controller;
 
+use AdminBundle\Form\Type\SubscriptionType;
+use AppBundle\Entity\Subscription;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class DefaultController
+ * @package AdminBundle\Controller
+ */
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="admin-panel")
+     * Lists all subscription entities.
      *
-     * @return Response
+     * @Route("/", name="subscription_index", methods={"GET"})
      */
     public function indexAction()
     {
-        return $this->render('@Admin/default/panel.html.twig');
+        $em = $this->getDoctrine()->getManager();
+
+        $subscriptions = $em->getRepository(Subscription::class)->findAll();
+
+        return $this->render('@Admin/subscription/index.html.twig', array(
+            'subscriptions' => $subscriptions,
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing subscription entity.
+     *
+     * @Route("/subscription/{id}", name="subscription_edit", methods={"GET", "POST"})
+     * @param Request $request
+     * @param Subscription $subscription
+     * @return RedirectResponse|Response
+     */
+    public function editAction(Request $request, Subscription $subscription)
+    {
+        $editForm = $this->createForm(SubscriptionType::class, $subscription);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('subscription_edit', array('id' => $subscription->getId()));
+        }
+
+        return $this->render('@Admin/subscription/edit.html.twig', array(
+            'subscription' => $subscription,
+            'edit_form' => $editForm->createView(),
+        ));
     }
 }
---
+++ src/AdminBundle/Form/Type/SubscriptionType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AdminBundle\Form\Type;
+
+use AppBundle\Entity\Subscription;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SubscriptionType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class, ['disabled' => true])
+            ->add('isActive');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Subscription::class
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'adminbundle_subscription';
+    }
+
+}
